cb66663c00bd1a716acd4a5e222284c95c0f3536
Root G <gorkemmulayim@icloud.com>
2016-10-06 17:32:05 +0300

    Added test for fewer array elements and fixed a potential bug.

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,13 @@
-def isDistinct(array, startIndex, endIndex):
+def isDistinct(array, index1, index2):
     """ 
         This method searches the given array from startIndex to endIndex
         for same elements. If all the elements are distinct in the given range,
         returns True, else returns False.
 
     Args:
-        array (list): Array to be iterater.
-        startIndex (int): Start index, can be negative for backwards search.
-        endIndex (int): End index, can be negative for backwards search.
+        array (list): Array to be iterated.
+        index1 (int): Start index, can be negative for backwards search.
+        index2 (int): End index, can be negative for backwards search.
 
     Returns:
         bool: The return value. True if all the elements are distinct, False otherwise.
@@ -17,20 +17,21 @@ def isDistinct(array, startIndex, endIndex):
             successfully if finds an answer before access the array with wrong index.
 
     """
+    startIndex = int(index1)
+    endIndex = int(index2)
+    if not isinstance(array, list):
+        return False
     if len(array) == 0:
         return False
-    if len(array) == 1:
-        return True
     if abs(startIndex) > len(array) or abs(endIndex) > len(array):
         raise IndexError
-    else:
-        if startIndex < 0:
-            startIndex += len(array)
-        if endIndex < 0:
-            endIndex += len(array)
+    if startIndex < 0:
+        startIndex += len(array)
+    if endIndex < 0:
+        endIndex += len(array)
     if startIndex == endIndex:
         return True
-    elif startIndex < endIndex:
+    if startIndex < endIndex:
         step = 1;
     else:
         step = -1;
--- test_algorithm.py
+++ test_algorithm.py
@@ -6,13 +6,16 @@ class TestContainsSameElement(unittest.TestCase):
         self.array = [10, 55, 4, 20, -6, 1, 4, 3, 3]
 
     def test_successful(self):
-        self.assertTrue(isDistinct(self.array, 7, 8))
+        self.assertFalse(isDistinct(self.array, 7, 9))
 
     def test_failed(self):
-        self.assertFalse(isDistinct(self.array, 0, 8))
+        self.assertFalse(isDistinct(self.array, 0, 9))
 
     def test_emptyArray(self):
         self.assertFalse(isDistinct([], 0, 99))
+
+    def test_oneElementInArray(self):
+        self.assertTrue(isDistinct([1], 0, 1))
 
     def test_whenIndexesAreSame(self):
         self.assertTrue(isDistinct(self.array, 0, 0))
@@ -22,6 +25,7 @@ class TestContainsSameElement(unittest.TestCase):
 
     def test_whenSecondIndexIsNegative(self):
         self.assertFalse(isDistinct(self.array, 0, -1))
+
     def test_whenIndexIsOutRange(self):
         self.assertRaises(IndexError, isDistinct, self.array, 100, 101)
 if __name__ == '__main__':
