8c9d0a7928ae30c205555b0eaeb2e29416fae98c
Root G <gorkemmulayim@icloud.com>
2016-10-06 08:26:45 +0300

    Update algorithm and it's unittest.

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,21 @@
 def containsSameElement(array, startIndex, endIndex):
-    if len(array) == 0:
-        return False;
-    for i in range(startIndex, endIndex):
-        for j in range(startIndex, endIndex):
-            if array[i] == array[j]:
-                return True;
-    return False;
+    if len(array) == 0 or len(array) == 1:
+        return False
+    if abs(startIndex) > len(array) or abs(endIndex) > len(array):
+        raise IndexError
+    else:
+        if startIndex < 0:
+            startIndex += len(array)
+        if endIndex < 0:
+            endIndex += len(array)
+    if startIndex == endIndex:
+        return False
+    elif startIndex < endIndex:
+        step = 1;
+    else:
+        step = -1;
+    for i in range(startIndex, endIndex, step):
+        for j in range(startIndex, endIndex, step):
+            if array[i] == array[j] and i != j:
+                return True
+    return False
--- test_algorithm.py
+++ test_algorithm.py
@@ -2,33 +2,25 @@ import unittest
 from algorithm import containsSameElement
 
 class TestContainsSameElement(unittest.TestCase):
-  def setUp(self):
-    self.array = [10, 55, 4, 20, -6, 1, 4, 3, 3]
+    def setUp(self):
+        self.array = [10, 55, 4, 20, -6, 1, 4, 3, 3]
 
-  def test_successful(self):
-    self.assertTrue(containsSameElement(self.array, len(self.array) - 2, len(self.array) - 1))
+    def test_successful(self):
+        self.assertTrue(containsSameElement(self.array, 0, len(self.array) - 1))
 
-  def test_failed(self):
-    self.assertFalse(containsSameElement(self.array, 0, 1))
+    def test_failed(self):
+        self.assertFalse(containsSameElement(self.array, 0, 1))
 
-  def test_emptyArray(self):
-    self.assertFalse(containsSameElement([], 0, 99))
+    def test_emptyArray(self):
+        self.assertFalse(containsSameElement([], 0, 99))
 
-  def test_whenIndexIsOutOfRange(self):
-    self.assertRaises(IndexError, containsSameElement(self.array, 0, 0))
+    def test_whenIndexesAreSame(self):
+        self.assertFalse(containsSameElement(self.array, 0, 0))
 
-  def test_whenIndexesAreSame(self):
-    self.assertFalse(containsSameElement(self.array, 0, 0))
+    def test_whenIndexIsNegative(self):
+        self.assertTrue(containsSameElement(self.array, -1, 0))
 
-  def test_whenArrayIsNone(self):
-    self.assertRaises(TypeError, containsSameElement(None, 0, 99))
-
-  def test_whenIndexIsNegative(self):
-    self.assertTrue(containsSameElement(self.array, -1, 0))
-
-  def test_searchingNoneFailed(self):
-    self.assertFailed(containsSameElement([None], 0, (self.array) - 1))
- 
 
 if __name__ == '__main__':
     unittest.main()
+
